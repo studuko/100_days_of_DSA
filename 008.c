80644d66ad7cffa2923d63ff3a7d69df93063cb0
studuko <neha2003jun@gmail.com>
2026-02-08 15:43:13 +0530

    008.c

---
+++ 008.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    printf("Enter a and b: ");
+    scanf("%d %d", &a, &b);
+
+    printf("%d^%d = %d", a, b, power(a, b));
+    return 0;
+}
