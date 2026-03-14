004a76d66d393e24778ffbd9ab5a2847d9be3b98
studuko <neha2003jun@gmail.com>
2026-03-14 14:20:40 +0530

    042.c

---
+++ 042.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#define MAX 100
+
+int queue[MAX], front = -1, rear = -1;
+int stack[MAX], top = -1;
+
+void enqueue(int x){
+    if(rear == MAX - 1){
+        printf("Queue Overflow\n");
+        return;
+    }
+    if(front == -1) front = 0;
+    queue[++rear] = x;
+}
+
+int dequeue(){
+    if(front == -1 || front > rear){
+        printf("Queue Underflow\n");
+        return -1;
+    }
+    return queue[front++];
+}
+
+void push(int x){
+    if(top == MAX - 1){
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = x;
+}
+
+int pop(){
+    if(top == -1){
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    return stack[top--];
+}
+
+void reverseQueue(){
+    while(front <= rear){
+        push(dequeue());
+    }
+
+    while(top != -1){
+        enqueue(pop());
+    }
+}
+
+void display(){
+    if(front == -1 || front > rear){
+        printf("Queue is empty\n");
+        return;
+    }
+
+    for(int i = front; i <= rear; i++){
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n, x;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    printf("Original Queue: ");
+    display();
+
+    reverseQueue();
+
+    printf("Reversed Queue: ");
+    display();
+
+    return 0;
+}
